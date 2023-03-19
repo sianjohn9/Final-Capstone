c8503dd00841d9a394d270a44b0574583ea72983
John Sian <115193516+sianjohn9@users.noreply.github.com>
2023-03-18 18:52:09 -0700

    Update 20210123122225_createReservationsTable.js

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -4,9 +4,9 @@ exports.up = function (knex) {
     table.string("first_name");
     table.string("last_name");
     table.string("mobile_number");
-    table.string("people");
     table.date("reservation_date");
     table.time("reservation_time");
+    table.integer("people");
     table.timestamps(true, true);
   });
 };
