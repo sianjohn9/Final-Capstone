2182fe20babb2e643ec1cf07043f2be6f79288d9
John Sian <115193516+sianjohn9@users.noreply.github.com>
2023-03-20 13:00:06 -0700

    Update ErrorAlert.js

--- front-end/src/layout/ErrorAlert.js
+++ front-end/src/layout/ErrorAlert.js
@@ -11,9 +11,7 @@ import React from "react";
 function ErrorAlert({ error }) {
   return (
     error && (
-      <div className="alert alert-danger m-2">
-        <ul>Error: {error.message}</ul>
-      </div>
+      <div className="alert alert-danger m-2">Error: {error.message}</div>
     )
   );
 }
